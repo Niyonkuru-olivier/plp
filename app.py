d16fe605c5111a4ee3802ebf653a68210f17f343
Niyonkuru-olivier <141176001+Niyonkuru-olivier@users.noreply.github.com>
2025-07-24 21:51:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,6 @@ def logincustomer():
     return render_template('login-customer.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
